a3c4004c45a6ddd7aab681231cb9f217a2409f91
Roderick Azzopardi <b43256@mcast.org.mt>
2017-11-07 12:28:18 +0100

    - Handled lose border collision - Tested out mouse position

---
+++ Block Breaker/Assets/Scripts/LoseBorderScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseBorderScript : MonoBehaviour {
+
+    LevelManager levelManager = new LevelManager();
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Triggered");
+        levelManager.LoadGame("Lose");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
