64b2f448a346d5f9b753225fbd888374c834dded
Roderick Azzopardi <b43256@mcast.org.mt>
2017-11-10 11:53:03 +0100

    - Fixed mouse position from pixels to game units - set the ball to be locked to the paddle position - once user clicks mouse button, ball flies up to start the game - created surrounding borders

---
+++ Block Breaker/Assets/Scripts/BallScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    public PaddleScript paddle;
+    Vector3 paddleBallPosDiff;
+    bool gameStart = false;
+
+	// Use this for initialization
+	void Start () {
+        paddleBallPosDiff = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!gameStart) {
+            this.transform.position = paddle.transform.position + paddleBallPosDiff;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !gameStart) {
+            gameStart = true;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+        }
+	}
+}
--- Block Breaker/Assets/Scripts/PaddleScript.cs
+++ Block Breaker/Assets/Scripts/PaddleScript.cs
@@ -12,11 +12,15 @@ public class PaddleScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        print(Input.mousePosition);
+        print(Input.mousePosition.x);
 
-        Vector3 newPaddlePos = new Vector3(Input.mousePosition.x, 
+        float mousePosInUnits = (Input.mousePosition.x / Screen.width * 16) - 8;
+
+        Vector3 newPaddlePos = new Vector3(mousePosInUnits, 
                                            this.transform.position.y, 
                                            this.transform.position.z);
+
+        newPaddlePos.x = Mathf.Clamp(mousePosInUnits, -7.5f, 7.5f);
 
         this.transform.position = newPaddlePos;
     }
